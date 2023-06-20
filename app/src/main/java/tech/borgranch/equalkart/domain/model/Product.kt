c089e9adb62999e892e597150f76e7e63d51d604
= <=>
2023-06-20 17:35:19 +0200

    :art: bring domain model in line with remote data source
    Signed-off-by: = <=>

--- app/src/main/java/tech/borgranch/equalkart/domain/model/Product.kt
+++ app/src/main/java/tech/borgranch/equalkart/domain/model/Product.kt
@@ -1,27 +1,18 @@
 package tech.borgranch.equalkart.domain.model
 
 data class Product(
-    val id: Int,
     val name: String,
     val price: Double,
-    val description: String,
-    val image: String,
 ) {
     companion object {
         fun empty(): Product {
-            return Product(0, "", 0.0, "", "")
+            return Product("", 0.0)
         }
     }
     fun editName(name: String): Product {
-        return Product(id, name, price, description, image)
+        return Product(name, price)
     }
     fun editPrice(price: Double): Product {
-        return Product(id, name, price, description, image)
-    }
-    fun editDescription(description: String): Product {
-        return Product(id, name, price, description, image)
-    }
-    fun editImage(image: String): Product {
-        return Product(id, name, price, description, image)
+        return Product(name, price)
     }
 }
--- app/src/main/java/tech/borgranch/equalkart/domain/model/ShoppingCart.kt
+++ app/src/main/java/tech/borgranch/equalkart/domain/model/ShoppingCart.kt
@@ -11,14 +11,16 @@ data class ShoppingCart(
     val tax: Double = 0.0,
     val total: Double = 0.0,
     val isEmpty: Boolean = items.isEmpty(),
-    val taxPercentage: Double = 0.21,
+    val taxPercentage: Double = 0.125,
 ) {
     /**
      * Add an item to the cart.
      * If the item does not exist and the quantity is positive, it is added.
      * If the item already exists, the quantity is updated.
      * If the quantity is negative, the item is removed.
+     * Synchronized to prevent multiple threads from accessing the cart at the same time.
      */
+    @Synchronized
     fun addItem(product: Product, quantity: Int = 1): ShoppingCart {
         // for an empty cart, only add items if quantity is positive
         if (this.isEmpty && quantity > 0) {
@@ -50,33 +52,56 @@ data class ShoppingCart(
      */
     fun removeItem(product: Product, quantity: Int = Int.MIN_VALUE): ShoppingCart {
         if (quantity == Int.MIN_VALUE) {
-            return(addItem(product, quantity))
+            return (addItem(product, quantity))
         }
-        return(addItem(product, quantity * -1))
+        return (addItem(product, quantity * -1))
     }
 
+    /**
+     * Empty the cart.
+     * Sets the items to an empty list and the subtotal, tax, and total to zero.
+     * Synchronized to prevent multiple threads from accessing the cart at the same time.
+     */
+    @Synchronized
     fun empty(): ShoppingCart {
         return ShoppingCart(emptyList(), 0.0, 0.0, 0.0)
     }
+
     private fun calculateTax(): ShoppingCart {
         return ShoppingCart(items, subtotal, currencyFormat(subtotal * taxPercentage), total)
     }
 
     private fun calculateSubtotal(): ShoppingCart {
-        return ShoppingCart(items, currencyFormat(items.sumOf { it.product.price * it.quantity }), tax, total)
+        return ShoppingCart(
+            items,
+            currencyFormat(items.sumOf { it.product.price * it.quantity }),
+            tax,
+            total,
+        )
     }
 
     private fun calculateTotal(): ShoppingCart {
         return ShoppingCart(items, subtotal, tax, currencyFormat(subtotal + tax))
     }
+
+    @Synchronized
     private fun calculate(): ShoppingCart {
         return calculateSubtotal().calculateTax().calculateTotal()
     }
 
+    @Synchronized
     fun checkout(): ShoppingCart {
         return empty()
     }
 
+    /**
+     * Get the quantity of a product in the cart.
+     * If the product does not exist, return zero.
+     * If the product exists, return the quantity.
+     * If no product is specified, return the total quantity of all products in the cart.
+     * @param product The product to get the quantity of.
+     * @return The quantity of the product in the cart, or zero if the product does not exist.
+     */
     fun getItemCount(product: Product? = null): Int {
         if (product == null) {
             return items.sumOf { it.quantity }
