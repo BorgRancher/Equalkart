086112d8ec5061fae09feea6eb203607ff2a963f
Shaun McDonald <borgrancher@icloud.com>
2023-07-07 12:00:24 +0200

    :sparkles: hilt-powered di for usecases
    Signed-off-by: Shaun McDonald <borgrancher@icloud.com>

--- app/src/main/java/tech/borgranch/equalkart/di/ProductUseCases.kt
+++ app/src/main/java/tech/borgranch/equalkart/di/ProductUseCases.kt
@@ -5,9 +5,13 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import tech.borgranch.equalkart.data.ProductRepository
+import tech.borgranch.equalkart.data.ShoppingCartRepository
 import tech.borgranch.equalkart.data.local.LocalDataSource
 import tech.borgranch.equalkart.data.remote.NetworkDataSource
+import tech.borgranch.equalkart.domain.usecases.AbandonCartUseCase
+import tech.borgranch.equalkart.domain.usecases.AddToCartUseCase
 import tech.borgranch.equalkart.domain.usecases.GetAllProductsUseCase
+import tech.borgranch.equalkart.domain.usecases.RemoveFromCartUseCase
 import javax.inject.Singleton
 
 @Module
@@ -23,9 +27,41 @@ object ProductUseCases {
     }
 
     @Provides
+    @Singleton
+    fun provideShoppingCartRepository(
+        localDataSource: LocalDataSource,
+        networkDataSource: NetworkDataSource,
+    ): ShoppingCartRepository {
+        return ShoppingCartRepository(localDataSource, networkDataSource)
+    }
+
+    @Provides
     fun provideGetAllProductsUseCase(
         productRepository: ProductRepository,
     ): GetAllProductsUseCase {
         return GetAllProductsUseCase(productRepository)
+    }
+
+    @Provides
+    fun provideAddToCartUseCase(
+        shoppingCartRepository: ShoppingCartRepository,
+        productRepository: ProductRepository,
+    ): AddToCartUseCase {
+        return AddToCartUseCase(shoppingCartRepository, productRepository)
+    }
+
+    @Provides
+    fun provideRemoveFromCartUseCase(
+        shoppingCartRepository: ShoppingCartRepository,
+        productRepository: ProductRepository,
+    ): RemoveFromCartUseCase {
+        return RemoveFromCartUseCase(shoppingCartRepository, productRepository)
+    }
+
+    @Provides
+    fun provideAbandonCartUseCase(
+        shoppingCartRepository: ShoppingCartRepository,
+    ): AbandonCartUseCase {
+        return AbandonCartUseCase(shoppingCartRepository)
     }
 }
